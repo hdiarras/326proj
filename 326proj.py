f9941e3cada888001f7f61bc83b5e752e748afe4
hdiarras <hdiarras@umd.edu>
2025-04-29 13:32:49 -0400

    Add files via upload

---
+++ 326proj.py
@@ -0,0 +1,69 @@
+class Gpa:
+    """
+    Calculate the grade poit average for students based on the UMD grading scale
+   
+    Attributes
+    ------------
+    course: list with course, course grade, and credits of each class 
+    
+    Returns
+    ---------
+    gpa: float representing grade point average
+    """
+    def __init__(self, course, credits):
+        pass
+    
+class Schedule:
+    """
+    Displays what days of the week the course meets, at what time, where, and when the final exams are.
+
+    Attributes
+    ------------
+    class_times:
+    hall:
+    exam_dates:
+    #if making this a list with imbedded lists is easier go for that
+    
+    Returns
+    -------------
+    """
+    
+    def __init__(self, class_times, hall, exam_dates):
+        pass
+
+class Course_details:
+    """
+    This displays the details of the course.
+    
+    Attributes
+    -----------
+    class_times:
+    hall:
+    exam_dates:
+     #if making this a list with imbedded lists is easier go for that
+    
+    Returns
+    --------
+    """
+    
+    def __init__(self, professor, students, ta):
+        pass
+    
+class Assignment_tracker:
+    """
+    This stores all the assignments for a course, their due dates, the weight of each assignment, and the grade received. 
+    
+    Attributes
+    -----------
+    professor:
+    students:
+    ta:
+    #if making this a list with imbedded lists is easier go for that
+    
+    Returns
+    --------
+
+    """
+    
+    def __init__(self, assignment, due_date, grade, weight):
+        pass
